781e9849b7fa2c97a1d33adeff4ba26875b3f36f
doyooni33 <your.email@example.com>
2025-06-29 17:12:58 +0900

    ??

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ settedmap = []
 
 pygame.init() 
 
+
+clock = pygame.time.Clock()
 screen_width = 480 
 screen_height = 320
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -12,9 +14,13 @@ pygame.display.set_caption("Game Title")
 
 running = True 
 while running:
+    df = clock.tick(60)
+
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             running = False
-
+        
+    pygame.draw.
+    pygame.display.flip()
 pygame.quit()
 print(settedmap)
---
+++ maptiles.py
@@ -0,0 +1,9 @@
+NOMAL_TILE = 0
+STAIRS_TILE = 1
+CLASSROOM_TILE = 2
+PLAYGROUND_TILE = 3
+
+class maptile:
+    def __init__(self):
+        print()
+    
