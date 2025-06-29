2204a8a001628b177eed0edd84cd3427c8258cfd
doyooni33 <your.email@example.com>
2025-06-29 16:00:59 +0900

    맵 셋팅팅

--- map/mapsetter.py
+++ map/mapsetter.py
@@ -1,6 +1,9 @@
 import pygame
 
-settedmap = []
+settedmap = [[0]*50]*50
+
+seting_x = 0
+seting_y = 0
 
 pygame.init() 
 
@@ -15,6 +18,21 @@ while running:
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_w:
+            print("W")
+            seting_y += 1
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+            print("A")
+            seting_y -= 1
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_a:
+            print("S")
+            seting_x -=1
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+            print("D") 
+            seting_x +=1
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            print("l")
+            settedmap[seting_x,seting_y] = 0
 
 pygame.quit()
 print(settedmap)
