ae509d5dc1d0e935a108d2f23113a63028fcb6b8
doyooni33 <your.email@example.com>
2025-07-02 23:35:24 +0900

    2025.7.2 바닥타일사진추가

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ while running:
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             running = False
+    
+    
+
     pygame.display.flip()
 pygame.quit()
 print(settedmap)
