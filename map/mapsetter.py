b05601b6b469d23f4c3f7322d791fbef6664d21d
doyooni33 <your.email@example.com>
2025-07-06 22:00:06 +0900

    사진추가,등록,맵만드는거 추가설정

--- map/mapsetter.py
+++ map/mapsetter.py
@@ -1,29 +1,38 @@
 import pygame
 
-settedmap = [[0]*50]*50
+settedmap = [[0]*10]*10
 
 seting_x = 0
 seting_y = 0
 
 def changetile(tile):
     if tile == 4:
-        tile==0
+        tile=0
     else :
         tile+=1
+    return tile
+
+tile_0 = pygame.image.load("./img/안보이는곳타일.jpg")
+tile_1 = pygame.image.load("./")
 
 pygame.init() 
 
 screen_width = 480 
 screen_height = 320
-screen = pygame.display.set_mode((screen_width, screen_height))
+size = [600, 480]
+screen = pygame.display.set_mode(size, pygame.RESIZABLE) 
 
-pygame.display.set_caption("Game Title")
+pygame.display.set_caption("out to ")
 
 running = True 
 while running:
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.VIDEORESIZE:
+            # 창 크기 조절 이벤트 처리
+            size = [event.w, event.h]
+            screen = pygame.display.set_mode(size, pygame.RESIZABLE)
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_w:
             print("W")
             seting_y += 1
@@ -37,8 +46,10 @@ while running:
             print("D") 
             seting_x +=1
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            print("l")
-            settedmap[seting_x,seting_y] = changetile(settedmap[seting_x,seting_y])
+           # print(settedmap)
+            print(settedmap[seting_x][seting_y]) 
+            settedmap[seting_x][seting_y] = changetile(settedmap[seting_x][seting_y])
+            print(settedmap[seting_x][seting_y])
     
-pygame.quit()
+pygame.quit() 
 print(settedmap)
